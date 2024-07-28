f6c15872222ef7504fc15fdf3e8ccd266e3007f0
Atlas <atlas@octocon.app>
2024-07-28 13:01:43 -0400

    feat: Allow deleting suggestions

--- lib/talkoyaki/commands/delete.ex
+++ lib/talkoyaki/commands/delete.ex
@@ -3,32 +3,64 @@ defmodule Talkoyaki.Commands.Delete do
 
   @behaviour Nosedrum.ApplicationCommand
 
+  alias Talkoyaki.Model.{
+    BugReport,
+    Suggestion
+  }
   alias Talkoyaki.Utils
-  alias Talkoyaki.Model.BugReport
 
   @impl true
-  def description, do: "Deletes this bug report."
+  def description, do: "Deletes this bug report or suggestion."
 
   @impl true
-  def command(%{member: %{roles: roles}, channel_id: thread_id}) do
+  def command(%{member: %{roles: roles}, channel_id: thread_id, channel: %{parent_id: forum_id}}) do
     mod_key_role_id = Application.get_env(:talkoyaki, :mod_key_role_id)
 
     if mod_key_role_id in roles do
-      case BugReport.get_bug_report_by_thread(thread_id) do
-        nil ->
-          Utils.error_embed("This bug report does not exist.")
+      cond do
+        forum_id == Application.get_env(:talkoyaki, :bug_reports_channel) ->
+          delete_bug_report(thread_id)
 
-        report ->
-          case BugReport.delete_bug_report(report.id) do
-            {:error, reason} ->
-              Utils.error_embed(reason)
+        forum_id == Application.get_env(:talkoyaki, :suggestions_channel) ->
+          delete_suggestion(thread_id)
 
-            :ok ->
-              Utils.success_embed("Successfully deleted this bug report!")
-          end
+        true ->
+          Utils.error_embed("This command can only be used in the channel of a bug report or suggestion.")
       end
     else
       Utils.error_embed("You do not have permission to use this command.")
+    end
+  end
+
+  defp delete_bug_report(thread_id) do
+    case BugReport.get_bug_report_by_thread(thread_id) do
+      nil ->
+        Utils.error_embed("This bug report does not exist.")
+
+      report ->
+        case BugReport.delete_bug_report(report.id) do
+          {:error, reason} ->
+            Utils.error_embed(reason)
+
+          :ok ->
+            Utils.success_embed("Successfully deleted this bug report!")
+        end
+    end
+  end
+
+  defp delete_suggestion(thread_id) do
+    case Suggestion.get_suggestion_by_thread(thread_id) do
+      nil ->
+        Utils.error_embed("This suggestion does not exist.")
+
+      suggestion ->
+        case Suggestion.delete_suggestion(suggestion.id) do
+          {:error, reason} ->
+            Utils.error_embed(reason)
+
+          :ok ->
+            Utils.success_embed("Successfully deleted this suggestion!")
+        end
     end
   end
 
